122ade4760c98cd4e2a2987da2ec91195e509854
Simon McLoughlin <simon.d.mcl@gmail.com>
2026-08-14 15:51:38 +0100

    bug fix: make constructor public

--- Sources/MvvmKit/UITableViewMutation.swift
+++ Sources/MvvmKit/UITableViewMutation.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 public struct UITableViewMutation {
-	public var insertedRows: [IndexPath] = []
-	public var deletedRows: [IndexPath] = []
-	public var reloadedRows: [IndexPath] = []
-	public var insertedSections: IndexSet = []
-	public var deletedSections: IndexSet = []
-	public var reloadedSections: IndexSet = []
-	public var animation: UITableView.RowAnimation = .automatic
+	public var insertedRows: [IndexPath]
+	public var deletedRows: [IndexPath]
+	public var reloadedRows: [IndexPath]
+	public var insertedSections: IndexSet
+	public var deletedSections: IndexSet
+	public var reloadedSections: IndexSet
+	public var animation: UITableView.RowAnimation
+	
+	public init(
+		insertedRows: [IndexPath] = [],
+		deletedRows: [IndexPath] = [],
+		reloadedRows: [IndexPath] = [],
+		insertedSections: IndexSet = [],
+		deletedSections: IndexSet = [],
+		reloadedSections: IndexSet = [],
+		animation: UITableView.RowAnimation = .automatic
+	) {
+		self.insertedRows = insertedRows
+		self.deletedRows = deletedRows
+		self.reloadedRows = reloadedRows
+		self.insertedSections = insertedSections
+		self.deletedSections = deletedSections
+		self.reloadedSections = reloadedSections
+		self.animation = animation
+	}
 }
